bd65767d6b45c18ad52e9eef7af6b932e01e0523
WangchukSherpa <95025561+WangchukSherpa@users.noreply.github.com>
2024-01-23 19:02:26 +0530

    Update Footer.jsx
    updated content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
   return (
     <div className={styles.container}>
-      <div> ©2023 JWSherpa. All rights reserved.</div>
+      <div> ©2024 JWSherpa. All rights reserved.</div>
       <div className={styles.social}>
         <Image src="/1.png" width={20} height={20} className={styles.icon} alt='Jig'/>
         <Image src="/2.png" width={20} height={20} className={styles.icon} alt='Jig'/>
